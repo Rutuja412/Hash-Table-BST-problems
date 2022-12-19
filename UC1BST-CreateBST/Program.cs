b8332a81d61b0c19c583eeb0b7a093a572eda093
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-19 13:36:53 +0530

    [Create BST]

---
+++ UC1BST-CreateBST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC1BST_CreateBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(56);
+            tree.AddNode(30);
+            tree.AddNode(70);
+            tree.Display();
+        }
+    }
+}
