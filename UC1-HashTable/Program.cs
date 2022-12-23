33cbb8c8f29ce168c64787fe3cb2af642d72666d
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-23 12:02:18 +0530

    [Ability Find Frequency]

---
+++ UC1-HashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC1_HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyMapNode<string, string> hashtable = new MyMapNode<string, string>(5);
+
+            hashtable.Add("0", "To");
+            hashtable.Add("1", "be");
+            hashtable.Add("2", "or");
+            hashtable.Add("3", "not");
+            hashtable.Add("4", "to");
+            hashtable.Add("5", "be");
+
+            string getword = hashtable.GetElement("4");
+             Console.WriteLine("4th index value is : " + getword);
+            
+        }
+    }
+}
